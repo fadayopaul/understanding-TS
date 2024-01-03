5efb0082cf5d74f7b59a7f668a5cab4d115d22ee
fadayopaul <paulfadayo@yahoo.com>
2024-01-03 05:42:27 +0100

    chore: :white_check_mark: working with tuple

--- app.ts
+++ app.ts
@@ -30,3 +30,21 @@ console.log(person3.hobbies);
 for (const hobby of person3.hobbies) {
   console.log(hobby.toUpperCase());
 }
+
+// Tuple
+const person4: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: "Paul",
+  age: 20,
+  hobbies: ["Sports", "Coding"],
+  role: [2, "Programmer"],
+};
+
+person4.role.push("admin");
+// person4.role[1] = 10;
+
+console.log(person4.role);
